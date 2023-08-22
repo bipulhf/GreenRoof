58292ba0c8530de0241918d1c27bb88d99fb284c
Md. Shahiduzzaman Bipul <bipulhf@gmail.com>
2023-08-22 19:56:29 +0600

    added maven projects

---
+++ backend/src/main/java/com/bankrupted/greenroof/GreenRoofApplication.java
@@ -0,0 +1,13 @@
+package com.bankrupted.greenroof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenRoofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenRoofApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bankrupted/greenroof/GreenRoofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankrupted.greenroof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenRoofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
