3ff60c77f38784beb1d406d8808ecbd6d5aa9e1c
bipulhf <bipulhf@gmail.com>
2023-09-03 23:37:18 +0600

    Added React Project Files

--- greenroof-frontend/src/main.tsx
+++ greenroof-frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routing/routes.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
